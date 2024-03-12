6035000b6a167c2351cdf075fce3519873ced0bc
Paul Ryan <paul@sharpred.com>
2024-03-12 14:11:56 +0000

    add dl script and remove out folder

---
+++ regions/dl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#wget https://download.geofabrik.de/europe/ireland-and-northern-ireland-latest.osm.pbf
+wget https://download.geofabrik.de/europe/great-britain-latest.osm.pbf
+wget https://download.geofabrik.de/africa/canary-islands-latest.osm.pbf
--- run.sh
+++ run.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
-#rm -fR out/gb/*
-
-#rm -fR images/*
-
-#rm -fR regions/*
-
-#cd ./regions && { curl -LO https://download.geofabrik.de/africa/canary-islands-latest.osm.pbf ; cd ..; }
-
-#cd ./regions && { curl -LO https://download.geofabrik.de/europe/great-britain-latest.osm.pbf ; cd ..; }
-
-#cd ./data && { curl -LO http://osm.thkukuk.de/data/sea-latest.zip ; cd ..; }
-
-#cd ./data && { curl -LO http://osm.thkukuk.de/data/bounds-latest.zip ; cd ..; }
 
 java -Xmx2000M -jar splitter.jar \
   regions/great-britain-latest.osm.pbf \
   --output-dir=out/gb \
   --mapid=83240001 \
-  --description="OSM GB Jan 2021"
+  --description="OSM GB May 2022"
 
 java -Xmx2000M -jar splitter.jar \
   regions/canary-islands-latest.osm.pbf \
   --output-dir=out/ci \
   --mapid=73240001 \
-  --description="OSM Canary Islands Jan 2021"
+  --description="OSM Canary Islands May 2022"
 
 
-java -Xmx2000M -jar mkgmap.jar \ 
- --route \
+java -Xmx2000M -jar mkgmap.jar --route \
  --add-pois-to-areas \
  --precomp-sea=data/sea-latest.zip \
  --bounds=data/bounds-latest.zip \
@@ -39,8 +25,7 @@ java -Xmx2000M -jar mkgmap.jar \
  --mapname=83240001 \
  out/gb/8324*.osm.pbf
 
- java -Xmx2000M -jar mkgmap.jar \ 
- --route \
+ java -Xmx2000M -jar mkgmap.jar --route \
  --add-pois-to-areas \
  --precomp-sea=data/sea-latest.zip \
  --bounds=data/bounds-latest.zip \
